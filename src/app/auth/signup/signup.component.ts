62fbbcd8aa04058c4070afafb7909ea904c938eb
MarkoCupic98 <67878561+MarkoCupic98@users.noreply.github.com>
2020-08-27 19:16:40 +0200

    Updated backend simulation

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -21,7 +21,7 @@ export class SignupComponent implements OnInit {
     if (!this.userService.getUser(form.value.email)) {
       this.errorExists = false;
       var newUser = this.userService.registerUser(form.value.email, form.value.password, form.value.fName, form.value.lName, form.value.date);
-      this.router.navigate(['']);
+      this.router.navigate(['/login']);
     } else {
       this.errorExists = true;
       this.errorText = "User with this email already exists."
