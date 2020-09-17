f2435168b70b0ea4987c9c6e733d5849c73fff7a
MarkoCupic98 <67878561+MarkoCupic98@users.noreply.github.com>
2020-09-17 19:54:41 +0200

    updated menu pictures

--- src/app/menu/menu.service.ts
+++ src/app/menu/menu.service.ts
@@ -8,12 +8,12 @@ export class MenuService {
         {id: '4', name: 'Chicken burger', price: '3$', picture: 'https://w.wallhaven.cc/full/49/wallhaven-4986wk.jpg', weight: '130g', size: 'Small'},
         {id: '5', name: 'Chicken burger', price: '6$', picture: 'https://w.wallhaven.cc/full/0w/wallhaven-0wlykr.jpg', weight: '180g', size: 'Medium'},
         {id: '6', name: 'Chicken burger', price: '9$', picture: 'https://images.unsplash.com/photo-1592422152375-f5165d20e1a8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80', weight: '240g', size: 'Large'},
-        {id: '7', name: 'Veggie burger', price: '5$', picture: 'https://w.wallhaven.cc/full/nz/wallhaven-nzomjo.jpg', weight: '120g', size: 'Small'},
-        {id: '8', name: 'Veggie burger', price: '8$', picture: 'https://w.wallhaven.cc/full/nz/wallhaven-nzomjo.jpg', weight: '170g', size: 'Medium'},
-        {id: '9', name: 'Veggie burger', price: '12$', picture: 'https://w.wallhaven.cc/full/nz/wallhaven-nzomjo.jpg', weight: '210g', size: 'Large'},
-        {id: '10', name: 'Pizza', price: '3$', picture: 'https://w.wallhaven.cc/full/nz/wallhaven-nzomjo.jpg', weight: '400g', size: 'Small'},
-        {id: '11', name: 'Pizza', price: '7$', picture: 'https://w.wallhaven.cc/full/nz/wallhaven-nzomjo.jpg', weight: '600g', size: 'Medium'},
-        {id: '12', name: 'Pizza', price: '11$', picture: 'https://w.wallhaven.cc/full/nz/wallhaven-nzomjo.jpg', weight: '850g', size: 'Large'}
+        {id: '7', name: 'Veggie burger', price: '5$', picture: 'https://w.wallhaven.cc/full/4l/wallhaven-4lkmol.jpg', weight: '120g', size: 'Small'},
+        {id: '8', name: 'Veggie burger', price: '8$', picture: 'https://w.wallhaven.cc/full/n6/wallhaven-n66ed7.jpg', weight: '170g', size: 'Medium'},
+        {id: '9', name: 'Veggie burger', price: '12$', picture: 'https://w.wallhaven.cc/full/45/wallhaven-45v3q9.jpg', weight: '210g', size: 'Large'},
+        {id: '10', name: 'Pizza', price: '3$', picture: 'https://w.wallhaven.cc/full/j5/wallhaven-j5ovyq.jpg', weight: '400g', size: 'Small'},
+        {id: '11', name: 'Pizza', price: '7$', picture: 'https://w.wallhaven.cc/full/1j/wallhaven-1j36m1.jpg', weight: '600g', size: 'Medium'},
+        {id: '12', name: 'Pizza', price: '11$', picture: 'https://w.wallhaven.cc/full/j5/wallhaven-j575dm.jpg', weight: '850g', size: 'Large'}
     ]
 
     getMenu() {
